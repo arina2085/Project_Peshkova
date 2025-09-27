dd131e99fd136687b083c820a11c3e593c4ed575
arina2085 <apeskova74@gmail.com>
2025-09-27 12:57:06 +0300

    практич 2

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,1 +1,7 @@
-print("hi git")
+number = int(input("Введите трехзначное число: "))
+num1 = number//100
+num2 = (number // 10) % 10
+num3 = number % 10
+sum = num1 + num2 + num3
+mult = num1 * num2 * num3
+print(f"Сумма: {sum}, произведение: {mult}")
