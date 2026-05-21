e27f50b7335079369a0d95e65ca4b71eaf489c63
arina2085 <apeskova74@gmail.com>
2026-05-21 17:31:56 +0300

    ляля

---
+++ PZ_13/PZ_13_1.py
@@ -0,0 +1,18 @@
+#В исходном текстовом файле (radio_stations.txt) найти все домены из URL-адресов (например, в URL-адресе http://stream.hoster.by:8081/pilotfm/audio/icecast.audio домен выделен полужирным)
+
+import re
+
+with open('radio_stations.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+domens = set()
+
+for line in lines:
+    reg = re.search('http://([^:/\s]+)', line)
+    if reg:
+        domen = reg.group(1) 
+        domens.add(domen)
+
+print("Найденные домены:")
+for domen in sorted(domens):
+    print(domen)
