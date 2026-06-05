135bce233aae32b0a51cf0a85b0fc7ccd92587fb
arina2085 <apeskova74@gmail.com>
2026-06-05 17:27:11 +0300

    уи

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -9,6 +9,12 @@ class Book:
     def get_info(self):
         return f"Название: {self.title}, Автор: {self.author}, Страниц: {self.pages}"
 
+    def read(self):
+        print(f"Читаем книгу '{self.title}' автора {self.author}.")
+        print(f"В книге {self.pages} страниц.")
+
+    def write(self, text):
+        print(f"Записываем в книгу '{self.title}': {text}")
 
 print("Создание книги")
 title = input("Введите название книги: ")
@@ -16,4 +22,5 @@ author = input("Введите автора: ")
 pages = int(input("Введите количество страниц: "))
 
 book = Book(title, author, pages)
+book.write(input("Введите новую запись: "))
 print(book.get_info())
